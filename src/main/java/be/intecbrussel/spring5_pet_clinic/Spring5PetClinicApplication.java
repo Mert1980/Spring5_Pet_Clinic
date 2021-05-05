5a0dd393dd3e8cf22b630adce8402c09be86b998
Mert <mertdemirok80@gmail.com>
2021-05-05 07:20:07 +0200

    add initial project

---
+++ src/main/java/be/intecbrussel/spring5_pet_clinic/Spring5PetClinicApplication.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.spring5_pet_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5PetClinicApplication.class, args);
+    }
+
+}
