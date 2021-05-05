31a5c858fdfa997ab4cd4ca923a5b6a6044abdbb
Emmsie <73163404+Emmsie@users.noreply.github.com>
2021-05-05 16:49:31 +0200

    Initial commit

---
+++ src/main/java/be/brussel/emmsie_pet_clinic/EmmsiePetClinicApplication.java
@@ -0,0 +1,13 @@
+package be.brussel.emmsie_pet_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmmsiePetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmmsiePetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/brussel/emmsie_pet_clinic/EmmsiePetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package be.brussel.emmsie_pet_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmmsiePetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
