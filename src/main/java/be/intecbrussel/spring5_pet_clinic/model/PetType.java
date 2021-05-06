695f0684906a0e85c4e8772f94d981325b2f2472
Mert <mertdemirok80@gmail.com>
2021-05-06 21:13:50 +0200

    add POJOs Closes #2

---
+++ src/main/java/be/intecbrussel/spring5_pet_clinic/model/PetType.java
@@ -0,0 +1,14 @@
+package be.intecbrussel.spring5_pet_clinic.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
