b44357545cb234966b3a1f2b5accba17675f270c
Mert <mertdemirok80@gmail.com>
2021-05-07 11:08:45 +0200

    create interfaces for pet clinic services. Closes #5

---
+++ pet-clinic-data/src/main/java/be/intecbrussel/services/PetService.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.services;
+
+import be.intecbrussel.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
