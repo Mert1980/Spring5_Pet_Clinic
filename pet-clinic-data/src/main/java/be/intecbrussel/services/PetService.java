a5a29cfa7a1a198d5e6687514f1446e974f342ba
Mert <mertdemirok80@gmail.com>
2021-05-08 12:26:34 +0200

    Refactor Service Interface to Common Base Interface. Closes #11

--- pet-clinic-data/src/main/java/be/intecbrussel/services/PetService.java
+++ pet-clinic-data/src/main/java/be/intecbrussel/services/PetService.java
@@ -2,12 +2,6 @@ package be.intecbrussel.services;
 
 import be.intecbrussel.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
