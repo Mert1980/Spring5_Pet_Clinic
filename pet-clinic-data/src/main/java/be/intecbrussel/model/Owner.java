9e5d279c8a787015334b71d59f1582b00b009856
Mert <mertdemirok80@gmail.com>
2021-05-20 15:27:06 +0200

    Convert Owners to JPA Entities. Closes #36

--- pet-clinic-data/src/main/java/be/intecbrussel/model/Owner.java
+++ pet-clinic-data/src/main/java/be/intecbrussel/model/Owner.java
@@ -1,13 +1,24 @@
 package be.intecbrussel.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    // if owner is deleted, all related pets will also be deleted
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
--- pet-clinic-data/src/main/java/be/intecbrussel/model/PetType.java
+++ pet-clinic-data/src/main/java/be/intecbrussel/model/PetType.java
@@ -1,7 +1,14 @@
 package be.intecbrussel.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {
@@ -11,4 +18,6 @@ public class PetType extends BaseEntity{
     public void setName(String name) {
         this.name = name;
     }
+
+
 }
