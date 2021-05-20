684bec1bea547decb77ff5bbae777ba4204a9768
Mert <mertdemirok80@gmail.com>
2021-05-20 14:24:56 +0200

    Implement Base Entity (JPA). Closes #35

--- pet-clinic-data/src/main/java/be/intecbrussel/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/be/intecbrussel/model/BaseEntity.java
@@ -1,8 +1,17 @@
 package be.intecbrussel.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+// Designates a class whose mapping information is applied to the entities that inherit from it.
+// A mapped superclass has no separate table defined for it.
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
