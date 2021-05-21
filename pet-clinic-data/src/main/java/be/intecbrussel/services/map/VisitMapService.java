53faf23f4143d247d90e20acba1fb8ee0f5f97c8
Mert <mertdemirok80@gmail.com>
2021-05-21 12:09:27 +0200

    Create Spring Data JPA Service for Visit. Closes #47

--- pet-clinic-data/src/main/java/be/intecbrussel/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/be/intecbrussel/services/map/VisitMapService.java
@@ -2,9 +2,11 @@ package be.intecbrussel.services.map;
 
 import be.intecbrussel.model.Visit;
 import be.intecbrussel.services.VisitService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
 
     @Override
